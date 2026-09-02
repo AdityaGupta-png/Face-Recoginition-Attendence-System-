99cf9390e3b66d1e853b7275ebec73c1260d3355
Aditya <adityagupta64837@gmail.com>
2026-09-02 14:04:27 +0530

    Complete the function of the train face

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ from PIL import Image,ImageTk
 from tkinter import messagebox   # importing the message box 
 import mysql.connector   # import mysql data base 
 import cv2       # Importing the opencv 
+import numpy as np
+import os
 
 
 # Making the class Face_Recogintion attendence system
@@ -33,7 +35,7 @@ class Train_Face:
         f_label_1.place(x=0,y=45,width=1280,height=250)
         
         # making the button 
-        b1_1 = Button(self.root,text="TRAIN DATA",font=("times new roman",35,"bold"),width=50,bg="blue",fg="white")
+        b1_1 = Button(self.root,text="TRAIN DATA",command=self.train_classifier,font=("times new roman",35,"bold"),width=50,bg="blue",fg="white")
         b1_1.place(x=0,y=295,width=1280,height=50)
         
         # Add the botttom images 
@@ -43,6 +45,40 @@ class Train_Face:
         
         f_label_1 = Label(self.root,image=self.photoimg_bottom)
         f_label_1.place(x=0,y=345,width=1280,height=300)
+        
+    def train_classifier(self):
+        data_dir = ("data")
+        path = [os.path.join(data_dir,file) for file in os.listdir(data_dir)]
+        
+        faces = []
+        ids = []
+        
+        for image in path :
+            img = Image.open(image).convert('L')  #Gray scal convert 
+            imageNp = np.array(img,'uint8')
+            
+            id = int(os.path.split(image)[1].split('.')[1])
+            
+            faces.append(imageNp)
+            ids.append(id)
+            
+            cv2.imshow("Training Face",imageNp)
+            cv2.waitKey(1) == 13
+            
+        ids = np.array(ids)
+        
+        # ====================== TRAIN THE CLASSIFIER AND SAVE ===================
+        # Create LBPH recognizer
+        clf = cv2.face.LBPHFaceRecognizer_create()
+
+        # Train with faces and IDs
+        clf.train(faces, np.array(ids))
+
+        # Save the trained model
+        clf.write("Classifier.xml")
+
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Result", "Training Datasets Completed!!!",parent=self.root) 
         
 
 # Making object 
