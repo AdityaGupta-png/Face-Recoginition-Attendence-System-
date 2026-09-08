1bb7c99e511d23d943cdd8fbbbae113c6c65e289
Aditya <adityagupta64837@gmail.com>
2026-09-08 21:20:55 +0530

    Making the scroll bar in the right frame

--- Attendence.py
+++ Attendence.py
@@ -73,70 +73,70 @@ class Attendence:
         studentId_label = Label(frame_1,text="StudentId:",font=("times new roman",12,"bold"),bg="white")
         studentId_label.grid(row=0,column=0,padx=10,pady=4)
         
-        studentId_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        studentId_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         studentId_entry.grid(row=0,column=1,padx=10,pady=4)
         
         # 2 --> Student Name 
         studentName_label = Label(frame_1,text="Student Name:",font=("times new roman",12,"bold"),bg="white")
         studentName_label.grid(row=0,column=2,padx=10,pady=4)
         
-        StudentName_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        StudentName_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         StudentName_entry.grid(row=0,column=3,padx=10,pady=4)
         
         # 3 --> current cousre 
         current_course_label = Label(frame_1,text="Current Course:",font=("times new roman",12,"bold"),bg="white")
         current_course_label.grid(row=1,column=0,padx=10,pady=4)
         
-        current_course_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        current_course_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         current_course_entry.grid(row=1,column=1,padx=10,pady=4)
         
         # 4 --> Department 
         department_label = Label(frame_1,text="Department:",font=("times new roman",12,"bold"),bg="white")
         department_label.grid(row=1,column=2,padx=10,pady=4)
         
-        department_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        department_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         department_entry.grid(row=1,column=3,padx=10,pady=4)
         
         # 5 --> Division 
         division_label = Label(frame_1,text="Division:",font=("times new roman",12,"bold"),bg="white")
         division_label.grid(row=2,column=0,padx=10,pady=4)
         
-        division_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        division_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         division_entry.grid(row=2,column=1,padx=10,pady=4)
         
         #  6 --> Roll No,. 
         roll_label = Label(frame_1,text="Roll No.:",font=("times new roman",12,"bold"),bg="white")
         roll_label.grid(row=2,column=2,padx=10,pady=4)
         
-        roll_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        roll_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         roll_entry.grid(row=2,column=3,padx=10,pady=4)
         
         # 7 --> Semester 
         Semester_label = Label(frame_1,text="Semester:",font=("times new roman",12,"bold"),bg="white")
         Semester_label.grid(row=3,column=0,padx=10,pady=4)
         
-        Semester_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        Semester_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         Semester_entry.grid(row=3,column=1,padx=10,pady=4)
         
         #  8 --> Date of birth
         dob_label = Label(frame_1,text="D.O.B.:",font=("times new roman",12,"bold"),bg="white")
         dob_label.grid(row=3,column=2,padx=10,pady=4)
         
-        dob_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        dob_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         dob_entry.grid(row=3,column=3,padx=10,pady=4)
         
         #  9 --> timing 
         timming_label = Label(frame_1,text="Timming:",font=("times new roman",12,"bold"),bg="white")
         timming_label.grid(row=4,column=0,padx=10,pady=4)
         
-        timming_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        timming_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         timming_entry.grid(row=4,column=1,padx=10,pady=4)
         
         # Date
         year_label = Label(frame_1,text="Date:",font=("times new roman",12,"bold"),bg="white")
         year_label.grid(row=4,column=2,padx=10,pady=4)
         
-        year_entry = ttk.Entry(frame_1,width=19,font=("times new roman",12,"bold"))
+        year_entry = ttk.Entry(frame_1,width=18,font=("times new roman",12,"bold"))
         year_entry.grid(row=4,column=3,padx=10,pady=4)
         
         # making the attendence label 
@@ -176,6 +176,66 @@ class Attendence:
         right_frame = LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details"
                                 ,font=("times new roman",12,"bold"))
         right_frame.place(x=635,y=0,width=630,height=435)
+        
+        # Making the frame and inside the frame we make the scrol bar 
+        table_frame = LabelFrame(right_frame,bd=2,bg="white",relief=RIDGE)
+        table_frame.place(x=0,y=0,width=625,height=410)
+        
+        # ================ SCROLL BAR AND TABLE ======================
+        # Making the scroll bar in the X 
+        scroll_x = ttk.Scrollbar(table_frame,orient=HORIZONTAL)
+        
+        # Making the scroll bar in the y
+        scroll_y = ttk.Scrollbar(table_frame,orient=VERTICAL)
+        
+        self.AttendenceReportTable = ttk.Treeview(table_frame,columns=("id",
+                                                               "name",
+                                                               "course",
+                                                               "dep",
+                                                               "div",
+                                                               "roll",
+                                                               "sem",
+                                                               "dob",
+                                                               "time",
+                                                               "date",
+                                                               "Attend_label"),
+                                          xscrollcommand=scroll_x.set,
+                                          yscrollcommand=scroll_y.set)
+        
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+    
+        self.AttendenceReportTable.heading("id",text="StudentId")
+        self.AttendenceReportTable.heading("name",text="Student Name")
+        self.AttendenceReportTable.heading("course",text="Current Course")
+        self.AttendenceReportTable.heading("dep",text="Department")
+        self.AttendenceReportTable.heading("div",text="Division")
+        self.AttendenceReportTable.heading("roll",text="Roll No.")
+        self.AttendenceReportTable.heading("sem",text="Semester")
+        self.AttendenceReportTable.heading("dob",text="Date Of Birth")
+        self.AttendenceReportTable.heading("time",text="Timming")
+        self.AttendenceReportTable.heading("date",text="Date")
+        self.AttendenceReportTable.heading("Attend_label",text="Attendence Label")
+        self.AttendenceReportTable["show"] = "headings"
+        
+        # Set the width of the every columns 
+        self.AttendenceReportTable.column("id",width=120)
+        self.AttendenceReportTable.column("name",width=120)
+        self.AttendenceReportTable.column("course",width=120)
+        self.AttendenceReportTable.column("dep",width=120)
+        self.AttendenceReportTable.column("div",width=120)
+        self.AttendenceReportTable.column("roll",width=120)
+        self.AttendenceReportTable.column("sem",width=120)
+        self.AttendenceReportTable.column("dob",width=120)
+        self.AttendenceReportTable.column("time",width=120)
+        self.AttendenceReportTable.column("date",width=120)
+        self.AttendenceReportTable.column("Attend_label",width=120)
+        
+        scroll_x.config(command=self.AttendenceReportTable.xview)
+        scroll_y.config(command=self.AttendenceReportTable.yview)
+        
+        self.AttendenceReportTable.pack(fill=BOTH,expand=1)
+        
         
         
         
